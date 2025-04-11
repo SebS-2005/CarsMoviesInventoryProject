d3c6e1bb8427ec404bdab5f0f44e8ea8d916e066
SebS-2005 <jssanchezr@academia.usbbog.edu.co>
2025-04-11 00:34:44 +0000

    commits

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS FLORES_ENTITI (
                                     id UUID DEFAULT RANDOM_UUID() PRIMARY KEY,
                                     FloresName VARCHAR(255) NOT NULL,
-                                    FloresColor VARCHAR(4) NOT NULL,
+                                    FloresColor VARCHAR(50) NOT NULL,
                                     Tamano VARCHAR NOT NULL
 );
