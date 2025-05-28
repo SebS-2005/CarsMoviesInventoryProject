7a6c72a509b9a9edfcaca1657dad9de76141ee17
SebS-2005 <jssanchezr@academia.usbbog.edu.co>
2025-05-28 04:29:40 +0000

    cambios

--- src/main/java/com/carsmoviesinventory/app/Entities/FloresEntities.java
+++ src/main/java/com/carsmoviesinventory/app/Entities/FloresEntities.java
@@ -1,36 +1,45 @@
 package com.carsmoviesinventory.app.Entities;
+
 import jakarta.validation.constraints.*;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.*;
 import java.util.UUID;
+
 @Entity
 @Table(name = "FLORES_ENTITI")
 @AllArgsConstructor
 @NoArgsConstructor
 public class FloresEntities {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @JsonProperty("id")
+    @Column(name = "id", nullable = false, updatable = false)
     private UUID id;
+
     @JsonProperty("floresName")
-    @NotBlank(message = "Flores names is required")
     @Size(min = 3, max = 100, message = "Flores name must be between 3 and 100 characters")
+    @Column(name = "flores_name", nullable = false, length = 100)
     private String floresName;
+
     @JsonProperty("floresColor")
-    @NotBlank(message = "Color is required")
-    @Size(min = 3, max = 100, message = "Flores Color must be between 3 and 100 characters")
+    @Size(min = 3, max = 100, message = "Flores color must be between 3 and 100 characters")
+    @Column(name = "flores_color", nullable = false, length = 100)
     private String floresColor;
+
     @JsonProperty("tamano")
-    @NotNull(message = "Tamano is required")
     @Size(min = 3, max = 100, message = "Flores size must be between 3 and 100 characters")
+    @Column(name = "tamano", nullable = false, length = 100)
     private String tamano;
+
     @PrePersist
     public void generateUUID() {
         if (id == null) {
             id = UUID.randomUUID();
         }
     }
+
     @Override
     public String toString() {
         return "FloresEntities{" +
@@ -40,24 +49,31 @@ public class FloresEntities {
                 ", tamano='" + tamano + '\'' +
                 '}';
     }
+
     public UUID getId() {
         return id;
     }
+
     public String getFloresName() {
         return floresName;
     }
+
     public void setFloresName(String floresName) {
         this.floresName = floresName;
     }
+
     public String getFloresColor() {
         return floresColor;
     }
+
     public void setFloresColor(String floresColor) {
         this.floresColor = floresColor;
     }
+
     public String getTamano() {
         return tamano;
     }
+
     public void setTamano(String tamano) {
         this.tamano = tamano;
     }
